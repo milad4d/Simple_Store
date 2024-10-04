bc842c4d6d67ad3dc37db0e4381480d3f6850e96
milad_takjoo <milad.takjoo@gmail.com>
2024-10-04 18:01:33 +0330

    header

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-$('#search-header').click(function(){
-    $('.responsive-search-header').css('left','0px');
-    // alert('dddd')
-});
-
-$('#close-search-header').click(function(){
-    $('.responsive-search-header').css('left','-1800px');
-    // alert('dddd')
-});
