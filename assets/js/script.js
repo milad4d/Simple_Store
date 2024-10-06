28c1200d388cf354ff9defc9576b56c7ee9dec93
milad_takjoo <milad.takjoo@gmail.com>
2024-10-06 21:44:25 +0330

    header

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$("#searchMobileButton").click(function () {
+    $(".search-mobile").css("left", "0px");
+  });
+  $('#searchCloseMobileButton').click(function(){
+    $(".search-mobile").css("left", "-550px");
+  });
